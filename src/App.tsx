5b7d7fdedf7c8178a1e3b00e3aa9e250577d88bc
null <null>
2025-12-16 17:09:04 -0500

    Some Basic Examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import withStyleHOC from './withStyleHOC';
+
+function Counter({start}: {start: number}) {
+  const [count, setCount] = useState(start);
+  return(
+    <>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </>
+  )
+}
+
+function Greetings({message}: {message: string}) {
+  return <h1>Hello, {message}</h1>
+}
 
 const App = () => {
+  const GreenCounter = withStyleHOC(Counter, {border: '1px solid green'});
+  const RedCounter = withStyleHOC(Counter, {border: '1px solid red'});
+  const PinkMsg = withStyleHOC(Greetings, {color: 'pink'});
+
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <PinkMsg message='HOC!' />
+      <GreenCounter start={5} />
+      <RedCounter start={7} />
     </div>
   );
 };
